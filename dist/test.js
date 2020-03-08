d43890cdb4793b13579d10ceca07165b3fbc9219
MyPoZi <kxx555u.u@gmail.com>
2020-03-08 15:45:11 +0900

    add: jsファイルをビルドに含む, 段階的に既存のJavaScriptからTSに移行したいときなどに使う

--- dist/test.js
+++ dist/test.js
@@ -1,5 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var sample_1 = require("./sample");
+var a = sample_1.sampleText;
+var b = sample_1.sampleFunction();
 function test(arg) {
     return arg;
 }
--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,11 @@
+import { sampleText, sampleFunction } from './sample'
+
+const a = sampleText
+const b = sampleFunction()
+
 export function test(arg: any) {
     return arg
 }
 export function test2() {
-    return { value: 'test2'}
+    return { value: 'test2' }
 }
