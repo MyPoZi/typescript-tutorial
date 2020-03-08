295a1a79d9d6690c2014d1e214a884d35d280da3
MyPoZi <kxx555u.u@gmail.com>
2020-03-08 17:46:55 +0900

    add: keyof Literal Union Typesで取得できる

--- src/test.ts
+++ src/test.ts
@@ -11,12 +11,31 @@ export function test2() {
 }
 
 export function test3() {
-    let myObject: {'foo': string } = {
-        foo: 'foo'
+    let myObject: { 'foo': string } = {
+        foo: ''
     }
     let anotherObject: typeof myObject = { foo: '' }
     anotherObject['foo'] = 'value'
     // anotherObject['foo'] = 1 // Error
     // anotherObject['bar'] = 'value' // Error!
     console.log(myObject)
+}
+
+export function keyofTest() {
+    const myObject = {
+        foo: 'FOO',
+        bar: 'BAR',
+        baz: 'BAZ'
+    }
+    type keyObject = {
+        foo: string
+        bar: string
+    }
+    let someKey: keyof keyObject
+    someKey = 'foo'
+
+    let myObjectKey: keyof typeof myObject
+    myObjectKey = 'bar'
+    myObjectKey = 'foo'
+    // myObjectkey = 'qux' // Error!
 }
