eeab9f57d44ea054f44d354c753be38bf4754c61
MyPoZi <kxx555u.u@gmail.com>
2020-03-08 20:15:39 +0900

    add: enum

--- dist/test.js
+++ dist/test.js
@@ -11,3 +11,59 @@ function test2() {
     return { value: 'test2' };
 }
 exports.test2 = test2;
+function test3() {
+    var myObject = {
+        foo: ''
+    };
+    var anotherObject = { foo: '' };
+    anotherObject['foo'] = 'value';
+    // anotherObject['foo'] = 1 // Error
+    // anotherObject['bar'] = 'value' // Error!
+    console.log(myObject);
+}
+exports.test3 = test3;
+function keyofTest() {
+    var myObject = {
+        foo: 'FOO',
+        bar: 'BAR',
+        baz: 'BAZ'
+    };
+    var someKey;
+    someKey = 'foo';
+    var myObjectKey;
+    myObjectKey = 'bar';
+    myObjectKey = 'foo';
+    // myObjectkey = 'qux' // Error!
+}
+exports.keyofTest = keyofTest;
+function Color2Code(color) {
+    switch (color) {
+        case 0 /* RED */:
+            return '#e74c3c';
+            break;
+        case 1 /* BLUE */:
+            return '#3498db';
+            break;
+        case 2 /* YELLOW */:
+            return '#f1c40f';
+            break;
+        case 3 /* GREEEN */:
+            return '#1abc9c';
+            break;
+        default: return '#000000';
+    }
+}
+exports.Color2Code = Color2Code;
+console.log(Color2Code(0 /* RED */)); // enumで型を宣言してあるため、補完として出る。また、文字列より間違いが少ない。
+console.log("#e74c3c" /* RED */); // これで良くない・・・？？
+function ColorString2Code(color) {
+    switch (color) {
+        case 'RED':
+            return "#e74c3c" /* RED */;
+        case 'BLUE':
+            return "#3498db" /* BLUE */;
+        default:
+            return "#000000" /* BLACK */;
+    }
+}
+exports.ColorString2Code = ColorString2Code;
--- src/test.ts
+++ src/test.ts
@@ -39,3 +39,46 @@ export function keyofTest() {
     myObjectKey = 'foo'
     // myObjectkey = 'qux' // Error!
 }
+
+const enum Color {
+    RED,
+    BLUE,
+    YELLOW,
+    GREEEN
+}
+
+export function Color2Code(color: Color): string {
+    switch (color) {
+        case Color.RED: return '#e74c3c'; break;
+        case Color.BLUE: return '#3498db'; break;
+        case Color.YELLOW: return '#f1c40f'; break;
+        case Color.GREEEN: return '#1abc9c'; break;
+        default: return '#000000'
+    }
+}
+console.log(Color2Code(Color.RED)) // enumで型を宣言してあるため、補完として出る。また、文字列より間違いが少ない。
+console.log(ColorSetCode.RED) // これで良くない・・・？？
+
+
+// コード内で使うなら、`ColorSetCode.RED`, stringで比較したい時は下のコードで良いのでは？色文字と16進数がまとまっているので個人的に好き
+
+const enum ColorSetCode {
+    RED = '#e74c3c',
+    BLUE = '#3498db',
+    YELLOW = '#f1c40f',
+    GREEEN = '#1abc9c',
+    BLACK = '#000000'
+}
+
+
+
+export function ColorString2Code(color: string): string { // case が文字列ではなく、ColorSetCode.RED.toStringみたいにkeyを文字列としたいな
+    switch (color) {
+        case 'RED':
+            return ColorSetCode.RED
+        case 'BLUE':
+            return ColorSetCode.BLUE
+        default:
+            return ColorSetCode.BLACK
+    }
+}
