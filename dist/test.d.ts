01352a6019cf44cc8a76dec59cc862152b071db3
MyPoZi <kxx555u.u@gmail.com>
2020-03-10 00:03:56 +0900

    add: Nullable型

--- dist/test.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function test(arg: any): any;
-export declare function test2(): {
-    value: string;
-};
--- dist/test/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function test(arg) {
-    return arg;
-}
-exports.test = test;
